5d5c1d0d909b7e00f7306d9a884af26c72d992f1
Ricardo Vermeltfoort <ricardovermeltfoort@gmail.com>
2015-06-10 16:28:28 +0200

    add mp3 player and methods to display it

--- Program.cs
+++ Program.cs
@@ -16,35 +16,36 @@ namespace SoundSharp
     }
 
     class Program
-	{
-		const int ALLOWED_AUTHENTICATION_ATTEMPTS = 3;
+    {
+        const int ALLOWED_AUTHENTICATION_ATTEMPTS = 3;
 
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Vul uw naam in:");
-			var name = Console.ReadLine();
-			Console.Clear();
-			var auth = Login();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vul uw naam in:");
+            var name = Console.ReadLine();
+            Console.Clear();
+            var auth = Login();
 
-			switch (auth)
-			{
-				case "NietOK":
-					Console.WriteLine("Het is niet gelukt om u in te loggen.");
-					break;
+            switch (auth)
+            {
+                case "NietOK":
+                    Console.WriteLine("Het is niet gelukt om u in te loggen.");
+                    break;
 
-				case "OK":
-					Console.WriteLine("Welkom bij SoundSharp, " + name + "!");
-					break;
+                case "OK":
+                    Console.WriteLine("Welkom bij SoundSharp, " + name + "!");
+                    ShowMenu();
+                    break;
 
-				default:
-					Console.WriteLine("U heeft niks ingevult, hierbij gaan wij er van uit dat u SoundSharp wilt afsluiten.");
-					break;
-			}
+                default:
+                    Console.WriteLine("U heeft niks ingevult, hierbij gaan wij er van uit dat u SoundSharp wilt afsluiten.");
+                    break;
+            }
 
-			/* Pause the script */
-			Console.WriteLine("Druk op Enter om door te gaan...");
-			Console.ReadLine();
-		}
+            /* Pause the script */
+            Console.WriteLine("Druk op Enter om door te gaan...");
+            Console.ReadLine();
+        }
 
         static void ShowMenu()
         {
@@ -66,6 +67,10 @@ namespace SoundSharp
 
                     switch (input.KeyChar)
                     {
+                        case '1':
+                            ShowMP3Player(1);
+                            break;
+
                         case '9':
                             Console.WriteLine("Tot ziens!");
                             break;
@@ -74,44 +79,70 @@ namespace SoundSharp
             }
         }
 
+        static void ShowMP3Player(int id)
+        {
+            var mp3Player = GetMP3Player(id);
+            Console.WriteLine(""); // Spacing
+            Console.WriteLine("- " + mp3Player.id + " ----------------------------------------");
+            Console.WriteLine("Merk: " + mp3Player.make);
+            Console.WriteLine("Model: " + mp3Player.model);
+            Console.WriteLine("Opslagcapaciteit: " + mp3Player.mbSize + "MB");
+            Console.WriteLine("Prijs: " + mp3Player.price);
+        }
+
         static MP3Player GetMP3Player(int id)
         {
             MP3Player mp3Player;
-            mp3Player.id = 1;
-            mp3Player.make = "GET technologies .inc";
-            mp3Player.model = "HF 410";
-            mp3Player.mbSize = 4096;
-            mp3Player.price = 129.95f;
+
+            switch (id)
+            {
+                case 1:
+                    mp3Player.id = 1;
+                    mp3Player.make = "GET technologies .inc";
+                    mp3Player.model = "HF 410";
+                    mp3Player.mbSize = 4096;
+                    mp3Player.price = 129.95f;
+                    break;
+
+                default:
+                    mp3Player.id = 0;
+                    mp3Player.make = "";
+                    mp3Player.model = "";
+                    mp3Player.mbSize = 0;
+                    mp3Player.price = 0;
+                    break;
+            }
+
             return mp3Player;
         }
 
-		static string Login()
-		{
-			var attemptsLeft = ALLOWED_AUTHENTICATION_ATTEMPTS;
+        static string Login()
+        {
+            var attemptsLeft = ALLOWED_AUTHENTICATION_ATTEMPTS;
 
-			while (true)
-			{
-				if (attemptsLeft == 1)
-					Console.WriteLine("(LET OP: Laatste poging!)");
-				else
-					Console.WriteLine("(Poging " + attemptsLeft + " van " + ALLOWED_AUTHENTICATION_ATTEMPTS + ")");
+            while (true)
+            {
+                if (attemptsLeft == 1)
+                    Console.WriteLine("(LET OP: Laatste poging!)");
+                else
+                    Console.WriteLine("(Poging " + attemptsLeft + " van " + ALLOWED_AUTHENTICATION_ATTEMPTS + ")");
 
-				Console.WriteLine("Vul het juiste wachtwoord in:");
-				var password = Console.ReadLine();
-				Console.Clear();
+                Console.WriteLine("Vul het juiste wachtwoord in:");
+                var password = Console.ReadLine();
+                Console.Clear();
 
-				if (password == "")
-					return "Onbekend";
-				else if (password == "SHARPSOUND")
-					return "OK";
+                if (password == "")
+                    return "Onbekend";
+                else if (password == "SHARPSOUND")
+                    return "OK";
 
-				if (--attemptsLeft <= 0)
-					break;
+                if (--attemptsLeft <= 0)
+                    break;
 
-				Console.WriteLine("Dit wachtwoord is onjuist, probeert u het opnieuw.");
-			}
+                Console.WriteLine("Dit wachtwoord is onjuist, probeert u het opnieuw.");
+            }
 
-			return "NietOK";
-		}
-	}
+            return "NietOK";
+        }
+    }
 }
